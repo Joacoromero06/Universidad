8fc05a835f084115daa833f3968df10415990b0a
Joacoromero06 <joaquinromeroignacio@gmail.com>
2025-05-16 14:34:40 -0300

    Commit PilaInt

--- AyED/JavaCL1/src/aplicaciones/PilaInt.java
+++ AyED/JavaCL1/src/aplicaciones/PilaInt.java
@@ -10,12 +10,26 @@ import recursos.OperacionesCL1;
 
 public class PilaInt {
     private OperacionesCL1 pila;
+
+    public PilaInt(){
+        this.pila=new PilaSLinkedList();
+    }
+
+
+    public PilaInt(int tam){
+        this.pila=new PilaArr(tam);
+            
+    }
+
+
     void Carga(){
         int n=Entrada.sc.nextInt();
         for(int i=0;i<=n;i++){
             this.pila.meter(Entrada.sc.nextInt());
         }
     }
+
+
     public String toString() {
         String str="";
         while(!this.pila.estaVacia()){
@@ -24,5 +38,16 @@ public class PilaInt {
             str+="\n";
         }
         return str;
+    }
+    
+    
+    public void Reemplaza(int bus,int nvo){
+        int aux;
+
+        while(!this.pila.estaVacia()){
+            aux=(int)this.pila.sacar();
+            if(aux==bus)
+
+        }
     }
 }
