9fe8c2242bfe2b363a92207b13da1f816c270f61
Joacoromero06 <joaquinromeroignacio@gmail.com>
2025-05-20 15:46:23 -0300

    Inecesario commit

--- AyED/JavaTADs/src/Test/TestNum.java
+++ AyED/JavaTADs/src/Test/TestNum.java
@@ -6,10 +6,9 @@ import TADs.Numero;
 public class TestNum {
     public static void main(String[] args) {
         Numero n1=new Numero(-15);Numero n2=new Numero(5);
-        
+
         ParOrdenado cr= Numero.CyR(n1,n2);
         System.out.println("Cociente y resto entre  "+n1+" con "+n2+" es "+ cr);
-
 
         Numero n3=new Numero();
         n3.MCD(n1,n2);
