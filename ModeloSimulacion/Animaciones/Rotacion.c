12b2092ab7d79ff1545143c7917f08f4e60b4a9e
Joacoromero06 <joaquinromeroignacio@gmail.com>
2025-05-29 20:37:33 -0300

    Rotacion cubo terminado

--- ModeloSimulacion/Animaciones/Rotacion.c
+++ ModeloSimulacion/Animaciones/Rotacion.c
@@ -14,11 +14,9 @@ typedef punto Tcuadrado[4];
 void LimpiaPantalla(Tpantalla);
 void MuestraPantalla(Tpantalla);
 void DibujaCuadrado(Tpantalla,Tcuadrado);
+void DibujaLinea(Tpantalla,punto,punto);
 int CoordX_IndMat(float);
 int CoordY_IndMat(float);
-
-
-//CUADRADO
 
 void RotaCuadrado(Tcuadrado,float);
 void CreaCuadrado(Tcuadrado);
@@ -31,16 +29,17 @@ int main(void){
     CreaCuadrado(micuadrado);
     float angulo=0;
     int c=0;
-    while(c<100){
+    while(c<15){
         LimpiaPantalla(miframe);
         DibujaCuadrado(miframe,micuadrado);
-        angulo+=25;
+        angulo+=2;
         RotaCuadrado(micuadrado,angulo);
         MuestraPantalla(miframe);
-        usleep(100000); // 100ms
+        usleep(1000000); // 
         system("cls"); // o system("cls") en Windows
         c++;
     }
+    MuestraPantalla(miframe);
     
 
     return 0;
@@ -62,13 +61,48 @@ void MuestraPantalla(Tpantalla frame){
 }
 void DibujaCuadrado(Tpantalla frame,Tcuadrado cuadrado){
     int i=0;
-    int x,y;
     for(i;i<=3;i++){
-        x=CoordX_IndMat(cuadrado[i].x);
-        y=CoordY_IndMat(cuadrado[i].y);
-        frame[y][x]='*';
+        DibujaLinea(frame,cuadrado[i],cuadrado[(i+1)%4]);
+        frame[CoordY_IndMat(cuadrado[i].y)][CoordX_IndMat(cuadrado[i].x)]='A';
     }
 }
+void DibujaLinea(Tpantalla frame,punto p0,punto p1) {
+    int x0 = CoordX_IndMat(p0.x);
+    int y0 = CoordY_IndMat(p0.y);
+    int x1 = CoordX_IndMat(p1.x);
+    int y1 = CoordY_IndMat(p1.y);
+
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int err = 0,pasox=1,pasoy=1;
+    if(x1-x0<0){pasox=-1;}
+    if(y1-y0<0){pasoy=-1;}
+
+    if (dx > dy) {
+        while (x0 != x1) {
+            frame[y0][x0] = '?';
+            err += dy;
+            if (err >= dx) {
+                y0 += pasoy;
+                err -= dx;
+            }
+            x0 += pasox;
+        }
+    }else {
+        while (y0 != y1) {
+            frame[y0][x0] = '?';
+            err += dx;
+            if (err >= dy) {
+                x0 += pasox;
+                err -= dy;
+            }
+            y0 += pasoy;
+        }
+    }
+    frame[y0][x0] = '*'; // último punto
+}
+
+
 int CoordX_IndMat(float x){
     return (int)round(x + ANCHO / 2);
 }
@@ -80,10 +114,10 @@ int CoordY_IndMat(float y){
 void CreaCuadrado(Tcuadrado cuadrado){
     punto vert1,vert2,vert3,vert4;
     
-    vert1.x=6;vert1.y=6;
-    vert2.x=-6;vert2.y=6;
-    vert3.x=-6;vert3.y=-6;
-    vert4.x=6;vert4.y=-6;
+    vert1.x=10;vert1.y=10;
+    vert2.x=-10;vert2.y=10;
+    vert3.x=-10;vert3.y=-10;
+    vert4.x=10;vert4.y=-10;
 
     cuadrado[0]=vert1;
     cuadrado[1]=vert2;
